b2d8c1101262da2156eddc30d154f932acbb67a2
Megha Thippanna <meghathippannah@gmail.com>
2024-01-29 23:20:04 +0530

    Third commit

--- app.py
+++ app.py
@@ -1,31 +1,28 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, jsonify
 
 obj= Flask(__name__)
 
 @obj.route('/')
 def Welcome():
-    return "Welcome buddy"
+    return "Welcome to Flask"
 
-@ob.route('/calci')
+@obj.route('/calci', methods=["GET"])
 def calculator():
-    operator= request.json(operator)
-    number1= request.json(number1)
-    number2= request.json(number2)
+    operator= request.json["operator"]
+    number1= request.json["number1"]
+    number2= request.json["number2"]
 
-if operator=='addition':
-    result=number1+number2
-    print(result)
-elif operator=="subraction":
-    result=number1-number2
-    print(result)
-elif operator=="multiplication":
-    result=number1*number2
-    print(result)
-elif operator=="division":
-    result=number1/number2
-    print(result)
-else:
-    print("Unknown Operator. Please check")
+    if operator=="addition":
+        result= int(number1) + int(number2)
+    elif operator=="subraction":
+        result=int(number1)- int(number2)
+    elif operator=="multiplication":
+        result=int(number1)* int(number2)
+    elif operator=="division":
+        result=int(number1)/ int(number2)
+    else:
+        result= "Error"
+    return "The operation is {} and the result is {}".format(operator, result)
 
 if __name__ == '__main__':
     obj.run()
