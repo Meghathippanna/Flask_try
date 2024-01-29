6c371c9071ad87b2e388a3f1d73f737302cb5d18
Megha Thippanna <meghathippannah@gmail.com>
2024-01-29 22:25:33 +0530

    second commit

--- app.py
+++ app.py
@@ -8,21 +8,24 @@ def Welcome():
 
 @ob.route('/calci')
 def calculator():
-    operator=
-    number1=
-    number2=
+    operator= request.json(operator)
+    number1= request.json(number1)
+    number2= request.json(number2)
 
 if operator=='addition':
     result=number1+number2
+    print(result)
 elif operator=="subraction":
     result=number1-number2
+    print(result)
 elif operator=="multiplication":
     result=number1*number2
+    print(result)
 elif operator=="division":
     result=number1/number2
+    print(result)
 else:
     print("Unknown Operator. Please check")
-
 
 if __name__ == '__main__':
     obj.run()
